e76fae13f14baf3b02287d77df32fe950cb6285b
JaehongDev <println@kakao.com>
2023-08-24 22:10:28 +0900

    Initial commit

---
+++ src/main/java/com/example/multimodulepractice/MultimodulePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.multimodulepractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultimodulePracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultimodulePracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/multimodulepractice/MultimodulePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.multimodulepractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultimodulePracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
